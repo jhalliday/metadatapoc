926bc05ab5bf583964927bef4bcb19e9c811b8d6
Zak Hassan <zak.hassan1010@gmail.com>
2016-08-26 10:01:32 -0400

    Moving JAX-RS to submodule: https://github.com/zmhassan/metadata-registry-rest.git

--- jaxrs/push.docker-registry.sh
+++
@@ -1,3 +0,0 @@
-sudo docker   build  --rm -t  metadata-registry-rest  .
-sudo docker tag  metadata-registry-rest $REGISTRY/redhatanalytics/metadata-registry-rest
-sudo docker push $REGISTRY/redhatanalytics/metadata-registry-rest
